9c12d87053a09d4e6bbbac3d89cb3f05d25d39bb
Matthew Hope <mhope@adaptavist.com>
2022-07-26 21:21:01 +0100

    Update service.pp
    added POC for amazon linux 2 support

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,13 +5,20 @@ class ntp::service inherits ntp {
   if ! ($ntp::service_ensure in [ 'running', 'stopped' ]) {
     fail('service_ensure parameter must be running or stopped')
   }
+  
+   # hack for Amazon Linux 2 service proider
+  if ($::operatingsystem == 'Amazon' and $::operatingsystemrelease == '2') {
+    $service_provider = "systemd"
+  } else {
+    $service_provider = $ntp::service_provider
+  }
 
   if $ntp::service_manage == true {
     service { 'ntp':
       ensure     => $ntp::service_ensure,
       enable     => $ntp::service_enable,
       name       => $ntp::service_name,
-      provider   => $ntp::service_provider,
+      provider   => $service_provider,
       hasstatus  => true,
       hasrestart => true,
     }
